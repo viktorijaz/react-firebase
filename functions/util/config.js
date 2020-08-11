a7e83328048d4aa364fbf1e8d5e7d1e9af0b07e3
viktorija <viktorija11@gmail.com>
2020-08-11 16:41:40 +0200

    initial firebase setup

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: 'AIzaSyAavC1G9BgMmX5SHJVaWxFV9vhZj9nyWcE',
+  authDomain: 'react-165214.firebaseapp.com',
+  databaseURL: 'https://react-165214.firebaseio.com',
+  projectId: 'react-165214',
+  storageBucket: 'react-165214.appspot.com',
+  messagingSenderId: '955794111834',
+  appId: '1:955794111834:web:f897fc9362cbe80cc10763',
+  measurementId: 'G-KL548ZETRC',
+};
